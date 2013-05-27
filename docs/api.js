2d04af0c6e951ac4ee95f85901449fc1dd632826
IlMattoDel93 <mattia.manzati@gmail.com>
2013-05-28 01:20:19 +0200

    starting docs

---
+++ docs/api.js
@@ -0,0 +1,10 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "InputElement",
+        "ResponsiveStage"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
