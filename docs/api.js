3d07ff90c3315e4e188e208d6bf224fb984063e0
IlMattoDel93 <mattia.manzati@gmail.com>
2013-06-26 11:15:09 +0200

    Starting Replication System, now it works on server side.

--- docs/api.js
+++ docs/api.js
@@ -1,9 +1,14 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "Entity",
+        "Factory",
         "InputElement",
+        "Joinable",
         "Logger",
+        "Player",
         "Preloader",
+        "Replicators",
         "ResponsiveStage",
         "Scene",
         "SceneManager"
---
+++ server.js
@@ -0,0 +1,21 @@
+var Loader = require(__dirname+'/lib/loader');
+var io = require('socket.io').listen(99);
+
+Loader(['Server/Factory', 'Server/Player', 'Server/Entity'], function(Factory, Player, Entity){
+	var e = new Entity();
+	setInterval(function(){
+		Factory.despawn(e);
+		e = new Entity();
+		e.join('main');
+		Factory.spawn(e);
+	}, 3000);
+	io.sockets.on('connection', function(socket){
+		var p = new Player(socket);
+		p.join('main');
+		console.log(p.channels);
+		Factory.subscribe(p);
+		socket.on('disconnect', function(){
+			Factory.unscribe(p);
+		});
+	});
+});
